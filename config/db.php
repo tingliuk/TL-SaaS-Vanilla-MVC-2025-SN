70fc51355dcdc542b8d6c1354a5a1fedf2dca491
Teck Poo Tan <china.liuting@163.com>
2025-03-17 08:35:13 +0800

    wip: Commence Framework
    - Create the db.php config file
    - Create helper.php file with functions

---
+++ config/db.php
@@ -0,0 +1,20 @@
+
+<?php  
+/**  
+ * Database Configuration File 
+ * 
+ * Filename:        db.php 
+ * Location:        config/ 
+ * Project:         XXX-SaaS-Vanilla-MVC-SN 
+ * Date Created:    DATE_CREATED 
+ * 
+ * Author:          YOUR NAME
+ * 
+ */  
+return [  
+    'host' => '127.0.0.1',  
+    'port' => 3306,  
+    'dbname' => 'tl_saas_vanilla_mvc',  
+    'username' => 'tl_saas_vanilla_mvc',  
+    'password' => 'Password1234'  
+];
